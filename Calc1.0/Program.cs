b46ca6c5477415aeb839c43b5089b87cb63e5f04
Daniele Hanelli <hanellidaniele@gmail.com>
2020-11-25 23:45:03 +0100

    Add project files.

---
+++ Calc1.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Operazioni;
+
+namespace Calc1._0
+{
+    class Calc1_0
+    {
+        public static void Main(string[] args)
+        {
+            double n1 = Calcoli.Inserisci();
+            string risposta = Calcoli.Operazione();
+            double n2 = Calcoli.Inserisci();
+
+            double? tot = Calcoli.Calcolo(n1, n2, risposta);
+
+            Console.WriteLine(tot);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Operazioni/Calcoli.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Operazioni
+{
+    public class Calcoli
+    {
+        public static double Inserisci()
+        {
+            double n;
+            Console.WriteLine("Inserisci il numero:");
+            n = Convert.ToDouble(Console.ReadLine());
+            return n;
+        }
+        public static string Operazione()
+        {
+            Console.WriteLine("operazione / operatore");
+            string risp = Console.ReadLine();
+            return risp;
+        }
+        public static double? Calcolo(double num1, double num2, string opz)
+        {
+            double? tot = null;
+
+            switch (opz)
+            {
+                case "somma":
+                case "Somma":
+                case "SOMMA":
+                case "+":
+                    tot = num1 + num2;
+                    break;
+                case "sottrazione":
+                case "Sottrazione":
+                case "SOTTRAZIONE":
+                case "-":
+                    tot = num1 - num2;
+                    break;
+                case "prodotto":
+                case "Prodotto":
+                case "PRODOTTO":
+                case "*":
+                    tot = num1 * num2;
+                    break;
+                case "divisione":
+                case "Divisione":
+                case "DIVISIONE":
+                case "/":
+                    tot = num1 / num2;
+                    break;
+                default:
+                    Errore();
+                    break;
+
+
+
+            }
+            return tot;
+        }
+
+        public static void Errore()
+        {
+            Console.WriteLine("operazione non consentita");
+        }
+    }
+}
+
+        
